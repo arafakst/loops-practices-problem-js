0abb97c922c09cdcedfd442f246dedac71533fa4
arafakst <arafakst@gmail.com>
2023-03-01 03:18:07 +0600

    revesion

--- revesion.js
+++ revesion.js
@@ -1,1 +1,25 @@
+//string type variable
 var bottle = 'water';
+var bottleColor = 'yellow';
+//number type variable
+var sunglassPrice = 120;
+var penColor = 'blue';
+var isExpensive = true;
+
+var tableItems = ['bottle', 'pen', 'mouse', 'sunglass', 'paper'];
+
+var item3 = tableItems[3];  // get item
+tableItems[4] = 'notebook';  // set item
+
+// find index
+var penIndex = tableItems.indexOf('pen');
+
+//push pop
+
+//conditional
+if(tableItems.length > 4){
+
+}
+else{
+    
+}
