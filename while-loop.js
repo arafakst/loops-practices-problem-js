8034d6939733ab153fda5a4e866bb8c3f2659f69
arafakst <arafakst@gmail.com>
2023-03-01 14:20:25 +0600

    comment

---
+++ while-loop.js
@@ -0,0 +1,21 @@
+/**
+ * 1. Loop Variable
+ * 2. condition inside while
+ * 3.Loop body
+ * 4.change the loop variable
+ */
+
+
+var roastGiven = 0;
+while (roastGiven < 7) {
+    console.log('Roast den,Please!')
+    // roastGiven = roastGiven + 1;
+    // roastGiven += 1;
+    console.log(roastGiven);
+    roastGiven++;
+}
+
+// var num = 0;
+// while (num < 5) {
+//     console.log('Hello');
+// }
