4701df472db44333a77d53dd6784da1402e43044
arafakst <arafakst@gmail.com>
2023-03-01 15:11:03 +0600

    for loop

---
+++ for-loop.js
@@ -0,0 +1,26 @@
+// for loop
+var roastGiven = 0;
+for(var roastGiven = 0; roastGiven < 7; roastGiven++){
+    console.log('Roasr den,Please');
+}
+
+//simple vesrsion of for loop
+
+/* console.log('Simple for loop');
+for(var i = 0; i < 7; i++){
+    console.log(i);
+} */
+
+// for(var i = 0; i <= 10; i++){
+//     console.log(i);
+// }
+
+//even number
+for(var i = 0; i <= 20; i+=2){
+    console.log(i);
+}
+
+// odd number  //i+=2---- i= i+2;
+for(var i = 1; i <= 20; i+=2){
+    console.log(i);
+}
